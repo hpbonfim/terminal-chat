765afc24838aadb3b72d54ce2daddc3a760bfcdb
Henrique Bonfim <hp_bonfim@hotmail.com>
2021-03-24 08:58:20 -0300

    add server side

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,17 @@
+/*
+node index.js
+--username hpbonfim
+--room sala01
+--hostUrl localhost
+*/
+
 import Events from 'events'
+import CliConfig from './components/cliConfig.js';
 import TerminalController from "./components/terminalController.js";
+
+const [nodePath, filePath, ...commands] = process.argv
+const config = CliConfig.parseArguments(commands)
+console.log(config);
 
 const componentEmitter = new Events()
 
