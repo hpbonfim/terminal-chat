f2e938b90e8da66459993a19320245f96c503068
Henrique Bonfim <hp_bonfim@hotmail.com>
2021-03-25 07:52:01 -0300

    config conection between client and server

--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,28 @@ node index.js
 */
 
 import Events from 'events'
-import CliConfig from './components/cliConfig.js';
-import TerminalController from "./components/terminalController.js";
+import CliConfig from './src/cliConfig.js';
+import EventManager from './src/eventManager.js';
+import SocketClient from './src/socker.js';
+import TerminalController from "./src/terminalController.js";
+
 
 const [nodePath, filePath, ...commands] = process.argv
 const config = CliConfig.parseArguments(commands)
-console.log(config);
 
 const componentEmitter = new Events()
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
+const eventManager = new EventManager({ componentEmitter, socketClient })
+const events = eventManager.getEvents()
+socketClient.attachEvents(events)
+
+const data = {
+    roomId: config.room,
+    userName: config.username
+}
+
+eventManager.joinRoomAndWaitForMessages(data)
 
 const controller = new TerminalController()
 await controller.initializeTable(componentEmitter)
